c25a00bbd574990bf04721e133861393e0f8075d
Luke Becker <beckerluke1@gmail.com>
2019-08-16 17:00:01 -0500

    Added sumbit button event listener to add task. Added handleTaskSumbit function and ajax request in separate POST function in client

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,33 @@
 console.log('in client');
+
+$(document).ready(() => {
+    setupClickListeners();
+});
+
+function setupClickListeners() {
+    $('#js-btn-add').on('click', handleTaskSubmit);
+}
+
+function handleTaskSubmit(event) {
+    // object being sent to server
+    let taskToSend = {
+        task: $('#user-task-entry').val(),
+    };
+
+    postTask(taskToSend);
+
+    taskToSend.task.val(''); // clear user input fields
+}
+
+function postTask(taskToSend) {
+    $.ajax({
+        type: 'POST',
+        url: '/tasks',
+        data: taskToSend
+    }).then((response) => {
+        console.log('Response from server: ', response);
+    }).catch((error) => {
+        console.log('Error in POST', error);
+        alert('Unable to add task');
+    });
+}
