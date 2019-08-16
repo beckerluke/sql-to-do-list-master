ef61aa2b58eb35d5029563fd3df0895b5d4b4f9d
Luke Becker <beckerluke1@gmail.com>
2019-08-16 16:09:44 -0500

    Installed body-parser, express, pg. Created basic structure. Connected router file to server and database.

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const PORT = 5000;
+
+// Import routes
+const taskRouter = require('./routes/task.router');
+
+app.use(express.static('server/public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use('/tasks', taskRouter);
+
+app.listen(PORT, () => {
+    console.log('Listening on port: ', PORT);
+});
