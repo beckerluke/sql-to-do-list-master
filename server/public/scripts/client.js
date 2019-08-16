6957391fb60d3446d76702905e5687d963aa070b
Luke Becker <beckerluke1@gmail.com>
2019-08-16 18:48:00 -0500

    Added working GET and POST routes on the server side

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -16,9 +16,23 @@ function handleTaskSubmit(event) {
 
     postTask(taskToSend);
 
-    taskToSend.task.val(''); // clear user input fields
+    $('#user-task-entry').val(''); // clear user input fields
 }
 
+function getTasks() {
+    $.ajax({
+        type: 'GET',
+        url: '/tasks'
+    }).then((response) => {
+        // receiving list of tasks from database
+        console.log(response);
+        // renderList(response);  
+    }).catch((error) => {
+        console.log('error in GET', error);
+    });
+}
+
+// sending new user input to server 
 function postTask(taskToSend) {
     $.ajax({
         type: 'POST',
@@ -26,6 +40,7 @@ function postTask(taskToSend) {
         data: taskToSend
     }).then((response) => {
         console.log('Response from server: ', response);
+        getTasks();
     }).catch((error) => {
         console.log('Error in POST', error);
         alert('Unable to add task');
--- server/routes/task.router.js
+++ server/routes/task.router.js
@@ -4,4 +4,39 @@ const taskRouter = express.Router();
 //Connect to Database
 const pool = require('../modules/pool');
 
+// GET route
+taskRouter.get('/', (req,res) => {
+    const queryText = `SELECT * FROM "tasks";`;
+    
+    pool.query(queryText)
+        .then((result) => {
+            res.send(result.rows);
+        })
+        .catch((err) => {
+            console.log('Error: ', err);
+            res.sendStatus(500);
+        });
+});
+
+
+
+
+//POST route
+taskRouter.post('/', (req,res) => {
+    let newTask = req.body;
+    const queryText = `INSERT INTO "tasks" ("task", "completed")
+    VALUES ($1, 'N');`
+
+    pool.query(queryText, [newTask.task])
+        .then((result) => {
+        res.sendStatus(201); 
+        })
+        .catch((err) => {
+            console.log('error in posting', err);
+            
+            res.sendStatus(500);
+        });
+});
+
+
 module.exports = taskRouter;
