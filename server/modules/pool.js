d9b3d7a9a03855784701acc83ea71b799c59e91a
Luke Becker <beckerluke1@gmail.com>
2019-08-19 15:44:59 -0500

    Adding pool configuration options

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,14 +1,38 @@
 
 const pg = require('pg');
-
 const Pool = pg.Pool;
-const pool = new Pool({
-    database: 'weekend-to-do-app',
-    host: 'localhost',
-    port: 5432,
-    max: 10,
-    idleTimeoutMillis: 30000,
-});
+const url = require('url');
+
+let config = {};
+
+if (process.env.DATABASE_URL) {
+    const params = url.parse(process.env.DATABASE_URL);
+    const auth = params.auth.split(':');
+
+    config = {
+        user: auth[0],
+        password: auth[1],
+        host: params.hostname,
+        port: params.port,
+        database: params.pathname.split('/')[1],
+        ssl: true,
+        max: 10,
+        idleTimeoutMillis: 30000,
+    };
+} else {
+    config = {
+            database: 'weekend-to-do-app',
+            host: 'localhost',
+            port: 5432,
+            max: 10,
+            idleTimeoutMillis: 30000,
+    };
+}
+
+const pool = new POOL(config); 
+
+
+
 
 pool.on('connect', () => {
     console.log('Pool is connected');
